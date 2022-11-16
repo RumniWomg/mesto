75e5c8589ba339de3b06a5db4b05edc3fdadee60
Yakovlev Denis <rumniwomg@yandex.ru>
2022-11-16 21:43:12 +0500

    аааа деадлфйн горит

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,27 @@ closeAuthorButton.addEventListener('click', () => {
     footerAuthors.classList.remove('footer__authors_active'); // И с окна
 });
 
+
+// Находим форму в DOM
+let formElement = document.querySelector('.')
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__field_1')
+let jobInput = document.querySelector('.popup__field_2')
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
