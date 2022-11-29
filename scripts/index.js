f1fb409c11583335b2a654d52484e5bed1d7a3ff
Yakovlev Denis <rumniwomg@yandex.ru>
2022-11-29 15:07:29 +0500

    popap clone

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,61 @@
-let popup = document.querySelector('.popup'); // Фон попап окна
-let openPopupButton = document.querySelector('.profile__edit-button'); // Кнопкa для показа окна
-let closePopupButton = document.querySelector('.popup__close-icon'); // Кнопка для скрытия окна
-let formElement = document.querySelector('.form-edit')
-let nameInput = document.querySelector('.popup__input_field_name')
-let jobInput = document.querySelector('.popup__input_field_aboutme')
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+
+// Попап для редактирования профиля
+const popupProfile = document.querySelector('.popup_profile'); // Фон попап окна
+const openPopupProfile = document.querySelector('.profile__edit-button'); // Кнопкa для показа окна
+const closePopupProfile = document.querySelector('.popup__close-icon_profile'); // Кнопка для скрытия окна
+const formElementProfile = document.querySelector('.profile__form-edit')
+const nameInputProfile = document.querySelector('.popup__input_field_name')
+const jobInputProfile = document.querySelector('.popup__input_field_aboutme')
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 
-let openPopup = function () {
-  popup.classList.add('popup_opened');
-  username.value = profileTitle.textContent;
-  aboutme.value = profileSubtitle.textContent;
+const openPopupProf = function () {
+  popupProfile.classList.add('popup_opened');
+  nameInputProfile.value = profileTitle.textContent;
+  jobInputProfile.value = profileSubtitle.textContent;
 }
 
-let closePopup = function () {
-  popup.classList.remove('popup_opened');
+const closePopupProf = function () {
+  popupProfile.classList.remove('popup_opened');
 }
 
-
-function formSubmitHandler (evt) {
+function formSubmitHandlerProfile (evt) {
   evt.preventDefault();
-  profileTitle.textContent = username.value;
-  profileSubtitle.textContent = aboutme.value;
+  profileTitle.textContent = nameInputProfile.value;
+  profileSubtitle.textContent = jobInputProfile.value;
+  closePopupProf();
+}
+
+openPopupProfile.addEventListener('click', openPopupProf);
+closePopupProfile.addEventListener('click', closePopupProf);
+formElementProfile.addEventListener('submit', formSubmitHandlerProfile);
+
+
+// Попап для добалвения карточки
+const popupCard = document.querySelector('.popup_card'); // Фон попап окна
+const openPopupCard = document.querySelector('.profile__add-button'); // Кнопкa для показа окна
+const closePopupCard = document.querySelector('.popup__close-icon_card'); // Кнопка для скрытия окна
+const formElementCard = document.querySelector('.form-edit')
+const nameInputCard = document.querySelector('.popup__input_field_name')
+const jobInputCard = document.querySelector('.popup__input_field_aboutme')
+
+
+const openCardPopup = function () {
+  popupCard.classList.add('popup_opened');
+}
+
+const closeCardPopup = function () {
+  popupCard.classList.remove('popup_opened');
+}
+
+function formSubmitHandlerCard (evt) {
+  evt.preventDefault();
+  
 
   closePopup();
 }
 
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+openPopupCard.addEventListener('click', openCardPopup);
+closePopupCard.addEventListener('click', closeCardPopup);
+formElementCard.addEventListener('submit', formSubmitHandlerCard);
